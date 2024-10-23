0a666efc9c0075cf7f76db40474fac444f7c6c65
Pierre <pierre.noel@becode.org>
2024-10-23 14:37:19 +0200

    update

---
+++ Solution/Iteration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Iteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //
+        }
+    }
+}
---
+++ Solution/Iteration/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    public class Solution
+    {
+       //            
+    }
+}
